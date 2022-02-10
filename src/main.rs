cebe66f4f86e3973310a356bc853d5f68dc30791
moinSebi <s.vorbrugg@googlemail.com>
2022-02-10 17:53:08 +0100

    Added essential files.

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+mod fasta;
+mod snp;
+mod bed;
+
+use std::io::stdin;
+use fastq::Parser;
+use crate::fasta::{read_fasta, read_fasta2, fasta_file};
+use crate::snp::snps;
+
+fn main() {
+    println!("Hello, world!");
+    eprintln!("Read fasta");
+    let mut fasta: fasta_file = fasta_file::new();
+    fasta.from_file("/home/svorbrugg_local/Rust/data/TAIR10.fasta");
+    //let fasta = read_fasta("/home/svorbrugg_local/Rust/data/TAIR10.fasta");
+    eprintln!("SNPs");
+    snps(&fasta, 0.001);
+}
+
+
+
+
+
+/*
+Input fasta and parameterfile or command line
+
+Strings are not replaceable
+1. Always rerun and write new file
+2. Store everything in char vector then change this
+
+snps easy =
+indels = remove or add sequence
+
+translocation = extract vector
+add vector
+
+
+
+vector os string
+
+Add command line
+Add SNP
+Think about reference structure for populaton struture (azbe later)
+add insertions and deletions
+add translocations
+add inversions
+tandem duplication
+distal duplication
+
+remove genomes (how to find distal and tandem duplications and then revert functionality)
+
+
+check difference tranlocation and translatation
+
+
+output
+fasta
+with rename if wanted
+bed file with output
+maybe vcf
+
+
+ */
